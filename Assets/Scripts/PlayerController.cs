211a17a50e358a4a7b70b6e525aadb57eec548ab
Wyatt Revalee <wyatt.revalee@gmail.com>
2024-05-13 22:23:25 -0400

    Initial scene and input manager created

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public event Action<Vector2> OnMovement;
+    public bool isPaused;
+    public Player player;
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnMove(InputValue value)
+    {
+        Debug.Log(value.Get<Vector2>());
+        OnMovement?.Invoke(value.Get<Vector2>());
+        if (!isPaused)
+        {
+            rb.velocity = value.Get<Vector2>() * (player.movementSpeed * player.movementSpeedModifer);
+        }
+    }
+}
