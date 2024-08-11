ce137e08fb0ffaeaf7a75bd2cda8f551236b20ee
Wyatt Revalee <wyatt.revalee@gmail.com>
2024-08-11 11:29:40 -0400

    Added stats to game over menu

--- Assets/Scripts/UI/Menus/GameOverMenu.cs
+++ Assets/Scripts/UI/Menus/GameOverMenu.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class GameOverMenu : MonoBehaviour
 {
 
+    public GameObject statPrefab;
+    public GameObject statPanel;
+
     public void OpenMainMenu()
     {
         SceneManager.LoadScene(0);
@@ -16,6 +19,10 @@ public class GameOverMenu : MonoBehaviour
         foreach(KeyValuePair<string, Stat> kvp in stats)
         {
             Debug.Log(string.Format("{0} : {1}", kvp.Key, kvp.Value.total));
+            GameObject newUIStat = Instantiate(statPrefab, statPanel.transform);
+            Stat newStat = new Stat();
+            newStat.total = kvp.Value.total;
+            newUIStat.GetComponent<UI_Stat>().SetStatInfo(kvp.Key, newStat);
         }
     }
 
